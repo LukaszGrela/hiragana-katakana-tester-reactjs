572d8903286e896b223c6487eaa076ef9691222e
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-01-07 16:47:24 +0000

    Removed test button

--- src/App.js
+++ src/App.js
@@ -6,21 +6,17 @@ import connect from 'react-redux/lib/connect/connect';
 
 import Splash from './pages/Splash';
 import AppView from './pages/AppView';
-import { withRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          !this.props.hasData ?
-            <Splash />
-            :
-            <AppView />
-        }
-        <button onClick={() => { 
-          this.props.history.push(`/page_${Math.random()}`);
-        }}>Random</button>
+      {
+        !this.props.hasData ? 
+        <Splash />
+        :
+        <AppView />
+      }
       </div>
     );
   }
@@ -31,4 +27,4 @@ const mapStateToProps = (state) => {
     hasData: state.data && state.data.length > 0
   }
 };
-export default withRouter(connect(mapStateToProps)(App));
+export default connect(mapStateToProps)(App);
