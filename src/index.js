f9678c33a0c110606f2c2b5f6da6449db1f1f8a5
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-01-24 22:44:47 +0000

    Added bowser for browser detection and adding style classes based on sniffing.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,28 @@ import './index.css';
 import App from './App';
 import hkTesterStore from "./store/HKTesterStore";
 
+((bowser, html) => {
+
+    if (bowser.name) {
+        html.className += ' ' + bowser.name;
+        html.className += ' ' + bowser.name + '-' + bowser.version;
+    }
+    if (bowser.osname) {
+        html.className += ' ' + bowser.osname;
+        html.className += ' ' + bowser.osname + '-' + bowser.osversion;
+    }
+
+    if (bowser.mobile) html.className += ' mobile';
+    if (bowser.tablet) html.className += ' tablet';
+    if (bowser.iphone) html.className += ' iphone';
+    if (bowser.ipad) html.className += ' ipad';
+    if (bowser.ipod) html.className += ' ipod';
+    if (bowser.windowsphone) html.className += ' windowsphone';
+
+
+
+})(require('bowser'), document.getElementsByTagName('html')[0]);
+
 ReactDOM.render(
     <Provider store={hkTesterStore}>
         <Router>
