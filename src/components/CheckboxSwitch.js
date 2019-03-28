2070c288681b40ac4621e8f5d1e91f658da88613
Lukasz Grela <lukasz.grela@gmail.com>
2019-03-28 13:24:51 +0000

    'dumb' component

--- src/components/CheckboxSwitch.js
+++ src/components/CheckboxSwitch.js
@@ -13,51 +13,43 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-import React, { Component } from 'react';
+import React from 'react';
 
-import './css/OnOffSwitch.css'
+import './css/OnOffSwitch.css';
 
-class CheckboxSwitch extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            selected: props.isChecked || false
-        }
-    }
-    
-    componentWillReceiveProps(nextProps) {
-        if(this.state.selected !== nextProps.isChecked) {
-            this.setState({selected:nextProps.isChecked});
-        }
-    }
-
-
-    render() {
-        return (
-            <div className={"onoffswitch ." + this.props.id}>
-                <input
-                    type="checkbox"
-                    name="onoffswitch"
-                    className="onoffswitch-checkbox"
-                    id={this.props.id}
-                    checked={this.state.selected}
-                    onChange={(e) => {
-                        //this.setState({ selected: !this.state.selected });
-                        this.props.onChecked && this.props.onChecked(!this.state.selected);
-                    }}
-                />
-                <label className="onoffswitch-label" htmlFor={this.props.id}>
-                    <span className="onoffswitch-inner">
-                        <span className="before">{this.props.labelOn || "ON"}</span><span className="after">{this.props.labelOff || "OFF"}</span>
-                    </span>
-                    <span className="onoffswitch-switch">
-                        <span className="before">{this.props.iconOn || "+"}</span><span className="after">{this.props.iconOff || "-"}</span>
-                    </span>
-                </label>
-            </div>
-        )
-    }
-}
+const CheckboxSwitch = ({
+  id,
+  isChecked,
+  onChecked,
+  labelOn,
+  labelOff,
+  iconOn,
+  iconOff,
+}) => {
+  return (
+    <div className={`onoffswitch ${id}`}>
+      <input
+        type="checkbox"
+        name="onoffswitch"
+        className="onoffswitch-checkbox"
+        id={id}
+        checked={isChecked}
+        onChange={e => {
+          onChecked && onChecked(!isChecked);
+        }}
+      />
+      <label className="onoffswitch-label" htmlFor={id}>
+        <span className="onoffswitch-inner">
+          <span className="before">{labelOn || 'ON'}</span>
+          <span className="after">{labelOff || 'OFF'}</span>
+        </span>
+        <span className="onoffswitch-switch">
+          <span className="before">{iconOn || '+'}</span>
+          <span className="after">{iconOff || '-'}</span>
+        </span>
+      </label>
+    </div>
+  );
+};
 
 export default CheckboxSwitch;
